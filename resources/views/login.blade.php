12313e7fdcea19d1786ef90d47836c5b9ee90890
Hancie123 <hanciewanemphago@gmail.com>
2023-09-06 21:55:39 +0545

    login page change

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -2,7 +2,7 @@
 <html lang="en">
 
 <head>
-    <title>Firewins</title>
+    <title>Firewinz</title>
     <link rel="icon" href="{{url('assets/images/icons.png')}}">
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
